88ccca2166c8aa8d0b32f6c13c4a2caf5731af76
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-03-30 16:06:02 -0400

    3-31-2021_4:05PMEST

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,55 +1,34 @@
+// from data.js
 var tableData = data;
+var tbody = d3.select("tbody")
 
-console.log(tableData);
-
-var tbody = d3.select("tbody");
-var clear_button = d3.select("#clear-btn");
-var date_form = d3.select("#date_form");
-
-tableData.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
+tableData.forEach(function(ufo) {
+   // console.log(ufo);
+    var row = tbody.append("tr")
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td")
+        cell.text(value)
+        // Log the key and value
+       // console.log(`Key: ${key} and Value ${value}`);
+      });   
   });
+var submit =d3.select("#filter-btn")
+submit.on("click",function(){
+    var inputid = d3.select("#datetime");
+    var inputvalue = inputid.property("value")
+    console.log (inputvalue)
+    var filterrow = tableData.filter(x => x.datetime === inputvalue)
+    console.log(filterrow);
+    tbody.html("")
+    filterrow.forEach(function(ufo) {
+        // console.log(ufo);
+         var row = tbody.append("tr")
+         Object.entries(ufo).forEach(([key, value]) => {
+             var cell = row.append("td")
+             cell.text(value)
+             // Log the key and value
+            // console.log(`Key: ${key} and Value ${value}`);
+           });   
 
-  
-clear_button.on("click", runClear);
-date_form.on("submit", runFilterDate);
-
-function runClear() {
-    d3.event.preventDefault();
-    tbody.html("");
-    tableData.forEach((sighting) => {
-        var row = tbody.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
-}
-
-
-function runFilterDate() {
-    d3.event.preventDefault();
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-    console.log(filteredData);
-
-    tbody.html("");
-
-    filteredData.forEach((sighting) => {
-        var row = tbody.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
-}
-
-
-
-
+})
+})
