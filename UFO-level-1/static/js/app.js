d95afe23f1ac16c98da1664c286fbcec811a9fc7
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-03-30 15:32:02 -0400

    3-31-2021_3:31PMEST

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+var clear_button = d3.select("#clear-btn");
+var date_form = d3.select("#date_form");
+
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+clear_button.on("click", runClear);
+date_form.on("submit", runFilterDate);
+
+function runClear() {
+    d3.event.preventDefault();
+    tbody.html("");
+    tableData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+}
+
+
+function runFilterDate() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    console.log(filteredData);
+
+    tbody.html("");
+
+    filteredData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+}
+
+
+
+
